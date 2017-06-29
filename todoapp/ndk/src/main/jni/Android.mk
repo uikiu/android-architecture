8ab47493e599e2d52f56c93c49bc611072fbc70c
uikiu <study_blogs@163.com>
2017-06-29 09:50:35 +0800

    修改Android.mk：删除重复的内容

--- todoapp/ndk/src/main/jni/Android.mk
+++ todoapp/ndk/src/main/jni/Android.mk
@@ -14,15 +14,6 @@
 # include $(BUILD_SHARED_LIBRARY)
 
 
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE    := hello-jni
-LOCAL_SRC_FILES := hello.cpp
-
-include $(BUILD_SHARED_LIBRARY)
-
 
 
 
