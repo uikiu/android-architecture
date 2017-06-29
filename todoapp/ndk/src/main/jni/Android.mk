80f7b0bf8c247f82113fdb080a52e2e9196c1d73
uikiu <study_blogs@163.com>
2017-06-29 14:59:02 +0800

    将注解取消

--- todoapp/ndk/src/main/jni/Android.mk
+++ todoapp/ndk/src/main/jni/Android.mk
@@ -1,17 +1,17 @@
 # 定义源文件在开发树中的位置。在这里，固件系统提供的宏函数my-dir将返回当前目录(包含Android.mk文件本身的目录)的路径。
-# LOCAL_PATH := $(call my-dir)
+ LOCAL_PATH := $(call my-dir)
 
 # 声明CLEAR_VARS变量
-# include $(CLEAR_VARS)
+ include $(CLEAR_VARS)
 
 # 定义module名称
-# LOCAL_MODULE := hello-jni
+ LOCAL_MODULE := hello-jni
 
 # 枚举源文件，以空格分割多个源文件
-# LOCAL_SRC_FILES:=hello.cpp
+ LOCAL_SRC_FILES:=hello.cpp
 
 # 将所有内容链接到一起
-# include $(BUILD_SHARED_LIBRARY)
+ include $(BUILD_SHARED_LIBRARY)
 
 
 
